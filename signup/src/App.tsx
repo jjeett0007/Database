0dd211282eeae2daff1fb06946e1fe0874a8a08e
jjeett0007 <88599825+jjeett0007@users.noreply.github.com>
2023-03-09 18:44:20 +0000

    sign up page

--- signup/src/App.tsx
+++ signup/src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function SignUpPage() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [picture, setPicture] = useState(null);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSurnameChange = (event) => {
+    setSurname(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMobileNumberChange = (event) => {
+    setMobileNumber(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePictureChange = (event) => {
+    setPicture(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("surname", surname);
+    formData.append("email", email);
+    formData.append("mobileNumber", mobileNumber);
+    formData.append("password", password);
+    formData.append("picture", picture);
+    // TODO: send the form data to the backend server
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={handleNameChange} />
+      </label>
+      <label>
+        Surname:
+        <input type="text" value={surname} onChange={handleSurnameChange} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <label>
+        Mobile Number:
+        <input type="tel" value={mobileNumber} onChange={handleMobileNumberChange} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <label>
+        Picture:
+        <input type="file" accept="image/*" onChange={handlePictureChange} />
+      </label>
+      <button type="submit">Sign Up</button>
+    </form>
   );
 }
 
-export default App;
+export default SignUpPage;
